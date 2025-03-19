b06be4cb4f86ceff6d17cb4405974c1bf0a0e8c1
siva0704 <vaibhavabg1234@gmail.com>
2025-03-19 14:18:02 +0530

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/edu-hub-connector-62">
         <AuthProvider>
           <App />
           <Toaster />
